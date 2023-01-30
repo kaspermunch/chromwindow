9d22f5587fdf888d0027513e002c86da9cbd4bf3
Kasper Munch <kaspermunch@birc.au.dk>
2023-01-30 17:39:34 +0100

    Updated conda.sh

--- conda.sh
+++ conda.sh
@@ -6,7 +6,11 @@ conda config --set anaconda_upload yes
 
 platform=`uname`
 if [ $platform == 'Darwin' ] ; then
-    plat='osx-64'
+    if [`uname -m` == 'arm64'] ; then
+        plat='osx-arm64'
+    else
+        plat='osx-64'
+    fi
 else
     plat='linux-64'
 fi
@@ -18,9 +22,11 @@ name=${PWD##*/}
 conda skeleton pypi --output-dir conda $name
 
 # for for each version of python
-for pythonversion in 3.6 3.7 3.8; do
+for pythonversion in 3.6 3.7 3.8 3.9 ; do
     conda-build --python $pythonversion conda/$name
 done
+
+mkdir -p outputdir
 
 # upload osx versions and convert to other architectures (assuming python only)
 for path in `find $CONDA_PREFIX/conda-bld/$plat/ -name "$name-[0-9]*.bz2"`; do
